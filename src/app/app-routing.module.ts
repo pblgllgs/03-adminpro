3cc15db2f8284d7a5244d8b6f411be09ce5fe05a
pbl.gllgs <pbl.gllgs@gmail.com>
2021-11-10 12:32:36 -0300

    Updates
    Routing and  lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
 
 const routes: Routes = [
-  {path : '', component: PagesComponent,
-    children: [
-      {path: 'dashboard', component: DashboardComponent},
-      {path: 'grafica1', component: Grafica1Component},
-      {path: 'progress', component: ProgressComponent},
-      {path: '', redirectTo: '/dashboard', pathMatch:'full'}
-    ]
+  {
+    path: 'pages',
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
   },
-  {path: 'register', component: RegisterComponent},
-  {path: 'login', component: LoginComponent},
-
-  {path: '**', component: NopagefoundComponent},
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {path: '', redirectTo: '/dashboard' , pathMatch: 'full'},
+  {path: '**', component: NopagefoundComponent}
 ];
 
 
@@ -30,7 +28,9 @@ const routes: Routes = [
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [
     RouterModule
