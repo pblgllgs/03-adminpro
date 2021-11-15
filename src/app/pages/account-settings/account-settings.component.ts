fc3050a4acbed6a1142996dfd34d05473ea5080f
pbl.gllgs <pbl.gllgs@gmail.com>
2021-11-15 11:45:19 -0300

    updates

---
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
+
+@Component({
+  selector: 'app-account-settings',
+  templateUrl: './account-settings.component.html',
+  styles: [
+  ]
+})
+export class AccountSettingsComponent implements OnInit {
+
+  
+
+  constructor(private ss:SettingsService) { }
+
+  ngOnInit(): void {
+    this.ss.checkCurrentTheme();
+  }
+
+  changeTheme(theme: string){
+    this.ss.changeTheme(theme);
+  }
+
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -10,6 +10,7 @@ import { ProgressComponent } from './progress/progress.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 
 
@@ -18,7 +19,8 @@ import { PagesComponent } from './pages.component';
     ProgressComponent,
     DashboardComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettingsComponent
   ],
   imports: [
     CommonModule,
@@ -31,7 +33,8 @@ import { PagesComponent } from './pages.component';
     ProgressComponent,
     DashboardComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettingsComponent
   ]
 })
 export class PagesModule { }
