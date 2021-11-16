011c4ae59221d75852efedf62a8fe62326cf7e70
pbl.gllgs <pbl.gllgs@gmail.com>
2021-11-16 13:08:39 -0300

    Update rxjs.component.ts
    observables

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Observable, interval, Subscription } from 'rxjs';
+import { retry, take, map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -6,11 +8,52 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class RxjsComponent implements OnInit {
 
-  constructor() { }
+export class RxjsComponent implements OnDestroy  {
 
-  ngOnInit(): void {
+  public intervalSubs:Subscription;
+
+  constructor() {
+    /* this.retornaObservable().pipe(
+      retry(1)
+    ).subscribe(
+      valor => console.log('Subs', valor),
+      (err) => console.warn('error:',err),
+      () => console.log('obs terminado')
+    ); */
+
+    this.intervalSubs = this.retornaIntervalo()
+      .subscribe(console.log)
+  }
+  
+  ngOnDestroy(): void {
+    this.intervalSubs.unsubscribe();
+  }
+
+  retornaIntervalo():Observable<number>{
+    return interval(100)
+      .pipe(
+        map(valor => valor + 1),
+        filter(valor => valor%2 == 0),
+        //take(10)
+      );
+  }
+
+  retornaObservable():Observable<number>{
+    let i = -1;
+    return new Observable<number>(observer => {
+      const intervalo = setInterval ( () => {
+        i++;
+        observer.next(i);
+        if(i=== 4){
+          clearInterval(intervalo);
+          observer.complete();
+        }
+        if(i === 2){
+          observer.error('i llego al valor de 2')
+        }
+      } , 1000)
+    });
   }
 
 }
