b836001d39ebaa634bfbfa5bdd8f4292f5c7b811
pbl.gllgs <pbl.gllgs@gmail.com>
2021-11-16 21:13:58 -0300

    Update breadcrumbs.component.ts

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,10 +1,7 @@
-import { Component } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-
-export interface IBreadCrumb {
-  label: string;
-  url: string;
-}
+import { Component, OnDestroy } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -12,8 +9,33 @@ export interface IBreadCrumb {
   styles: [
   ]
 })
-export class BreadcrumbsComponent{
+export class BreadcrumbsComponent implements OnDestroy {
 
-  constructor() {
+  public titulo: string;
+  public tituloSubs$: Subscription;
+
+  
+  constructor( private router: Router) {
+
+   /*  this.tituloSubs$ = this.getArgumentosRuta()
+                        .subscribe( ({ titulo }) => {
+                            this.titulo = titulo;
+                            document.title = `AdminPro - ${ titulo }`;
+                        }); */
   }
+  ngOnDestroy(): void {
+    /* this.tituloSubs$.unsubscribe(); */
+  }
+
+
+  /* getArgumentosRuta() {
+    return this.router.events
+      .pipe(
+        filter( event => event instanceof ActivationEnd ),
+        filter( (event: ActivationEnd) => event.snapshot.firstChild === null  ),
+        map( (event: ActivationEnd) => event.snapshot.data )
+      );
+  } */
+
+
 }
