9997cf184ed2734e2c1880119d6e82a0c239ecef
pbl.gllgs <pbl.gllgs@gmail.com>
2021-12-02 23:42:33 -0300

    delete and update img

--- src/app/pages/mantenimientos/medicos/medicos.component.ts
+++ src/app/pages/mantenimientos/medicos/medicos.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Medico } from '../../../models/medico.model';
 import { MedicoService } from '../../../services/medico.service';
+import Swal from 'sweetalert2';
+import { ToastService } from '../../../services/toast.service';
+import { delay } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { ModalImageComponent } from '../../../components/modal-image/modal-image.component';
+import { ModalImagenService } from '../../../services/modal-imagen.service';
 
 @Component({
   selector: 'app-medicos',
@@ -13,13 +19,25 @@ export class MedicosComponent implements OnInit {
   public medicos:Medico[]= [];
   public medicosTemp:Medico[]= [];
   public cargando:boolean = false;
+  private imgSubs:Subscription;
 
   constructor(
-    private medicoService:MedicoService
+    private medicoService:MedicoService,
+    private toast:ToastService,
+    private modalImageService:ModalImagenService
   ) { }
 
   ngOnInit(): void {
     this.cargarMedicos();
+    this.imgSubs = this.modalImageService.nuevaImagen
+      .pipe(
+        delay(300)
+      )
+      .subscribe(
+        img => {
+          this.cargarMedicos();
+          
+        });
   }
 
   cargarMedicos() {
@@ -32,6 +50,35 @@ export class MedicosComponent implements OnInit {
         }
         this.medicosTemp = medicos;
       })
+  }
+
+  eliminarMedico(medico: Medico) {
+    Swal.fire({
+      title: 'Desea eliminar?',
+      text: "Esto no se puede revertir!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, borrar!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.medicoService.borrarMedico(medico._id)
+          .subscribe(
+            (resp) => {
+              this.toast.toast('success', 'Medico eliminado');
+              this.cargarMedicos();
+            },
+            (err) => {
+              this.toast.toast('error', 'Datos no fueron Eliminados')
+            }
+          );
+      }
+    })
+  }
+
+  abrirModal(medico:Medico){
+    this.modalImageService.abrirModal('medicos',medico._id,medico.img);
   }
 
 }
